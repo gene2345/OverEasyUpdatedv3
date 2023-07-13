1a6a9286469ea9beff0127967c6eb42a58711ace
gene2345 <leegeneee@rocketmail.com>
2023-07-13 15:48:25 +0800

    zz

--- website/static/index.js
+++
@@ -1,6 +0,0 @@
-function deleteNote(noteID) {
-    fetch('/deleteNote', {
-        method : 'POST',
-        body: JSON.stringify({ noteID : noteID})
-    }).then((_res) => { window.location.href = "/"});
-}
